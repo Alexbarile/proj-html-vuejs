536a06c6c696271860acd0a2d4d16db76a8e44c6
Alexbarile <alexbarile93fg@yahoo.it>
2023-01-23 16:27:54 +0100

    stilizzazione jumbotron

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -47,6 +47,9 @@ export default {
 <template>
     <header>
         <div class="container-100">
+
+            <!-- logo e navbar -->
+
             <div class="container-80">
                 <div class="row-header">
                     <div class="col">
@@ -57,13 +60,38 @@ export default {
                     <div class="col">
                         <nav>
                             <ul>
-                                <li v-for="(item, index) in navbar" :key="index" @click="selectedItem(index)" :class="(activeItem === index) ? 'active' : ''">
-                                    <a :href="item.url">
+                                <li v-for="(item, index) in navbar" :key="index" >
+                                    <a :href="item.url" class="item.active ? 'active : ''">
                                         {{ item.label }}
                                     </a>
                                 </li>
                             </ul>
                         </nav>
+                    </div>
+                </div>
+            </div>
+
+            <!-- jumbotron -->
+
+            <div class="row">
+                <div class="col-30">
+                    <div class="first-line">
+                        <div class="hr"></div>
+                        <h4>always the best way you need it</h4>
+                    </div>
+                    <h1>THE BEST BUSINESS CONSULTING</h1>
+                    <div class="third-line">
+                        <button class="button">
+                            read more
+                        </button>
+                        <i class="fa-regular fa-circle-play"></i>
+                    </div>
+
+                </div>
+
+                <div class="col-70">
+                    <div class="jumbo">
+                        <img src="../../public/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg" alt="jumbo">
                     </div>
 
                 </div>
@@ -77,11 +105,12 @@ export default {
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
 
+    // logo e navbar
     .row-header{
         @include flex;
         @include align-center;
         @include justify-between;
-        padding-top: 20px;
+        padding: 20px 0 20px 0;
 
         .logo{
             img{
@@ -95,12 +124,97 @@ export default {
                 list-style-type: none;
 
                 li{
-                    padding: 10px;
-                    
+                    padding: 13px;
+
                     a{
                         text-decoration: none;
                         color: #AAAAAA;
+
+                        &.active, &:hover{
+                            color: white;
+                            border-top: 2px solid white;
+                            padding-top: 30px;
+                        }
                     }
+                }
+            }
+        }
+    }
+
+    // jumbotron
+
+    .container-100{
+        .row{
+            @include flex;
+            width: 100%;
+            height: 100%;
+
+            .col-30{
+                width: 30%;
+                background-color: #1F1E38;
+                height: 1000px;
+
+                .first-line{
+                    @include flex;
+                    
+                    .hr{
+                       border-bottom: 2px solid #39FFBF;
+                       width: 40px;
+                       margin-right: 30px;
+                          
+                    }
+
+                    h4{
+                        @include upperCase;
+                        color: rgb(254, 230, 230);
+                        font-weight: 200;
+                    }
+                }
+
+                h1{
+                    color: white;
+                    font-size: 80px;
+                    line-height: 75px;
+                    padding: 30px 0 40px 0;
+                }
+
+                .third-line{
+                    @include flex;
+                    @include align-center;
+
+                    button{
+                        padding: 15px 35px 15px 35px;
+                        background-color: #39FFBF;
+                        @include upperCase;
+                        font-weight: 100;
+                    }
+
+                    i{
+                        color: #39FFBF;
+
+                        .fa-circle-play{
+                            padding: 60px;
+                        }
+                    }
+                }
+
+                
+
+            }
+
+            .col-70{
+                width: 70%;
+                background-color: blue;
+                height: 300px;
+
+                .jumbo{
+                    img{
+                        width: 100%;
+                        height: 1000px;
+                        object-fit: cover;
+                        object-position: top
+                    }
+
                 }
             }
         }
