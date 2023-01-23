c0ba2e1f1077fb74709d2b12595b03dc6338cc81
Alexbarile <alexbarile93fg@yahoo.it>
2023-01-23 12:15:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import { store } from './store';
+
+export default{
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+
+</style>
